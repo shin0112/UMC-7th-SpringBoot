2638677ab5401ecbc5a1b8562a2ee22d94658e87
shin0112 <wnslcosltimo12@gmail.com>
2024-09-26 21:36:12 +0900

    init spring

---
+++ src/test/java/com/umc/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
