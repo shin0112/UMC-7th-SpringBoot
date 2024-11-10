6b9b76d9b47b53b1afd6911aa6917c9f286fc323
shin0112 <wnslcosltimo12@gmail.com>
2024-11-10 19:56:37 +0900

    feat: sout test data code

--- src/main/java/com/umc/study/StudyApplication.java
+++ src/main/java/com/umc/study/StudyApplication.java
@@ -1,7 +1,11 @@
 package com.umc.study;
 
+import com.umc.study.service.store.StoreQueryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,4 +16,22 @@ public class StudyApplication {
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(ApplicationContext context) {
+		return args -> {
+			StoreQueryService storeQueryService = context.getBean(StoreQueryService.class);
+
+			// 파라미터 값 설정
+			String name = "요아정";
+			Float score = 4.0f;
+
+			// 쿼리 메서드 호출 및 쿼리 문자열과 파라미터 출력
+			System.out.println("Executing findStoresByNameAndScore with parameters:");
+			System.out.println("Name: " + name);
+			System.out.println("Score: " + score);
+
+			storeQueryService.findStoreListByNameAndScore(name, score)
+				.forEach(System.out::println);
+		};
+	}
 }
