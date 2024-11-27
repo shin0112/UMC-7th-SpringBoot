5c82ea27e7cdca162efbc3d996acaf5a3825f6dd
shin0112 <wnslcosltimo12@gmail.com>
2024-11-27 21:49:45 +0900

    feat: 미션

--- src/main/java/com/umc/study/repository/mission/MissionRepository.java
+++ src/main/java/com/umc/study/repository/mission/MissionRepository.java
@@ -1,8 +1,17 @@
 package com.umc.study.repository.mission;
 
 import com.umc.study.domain.Mission;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    @Query("SELECT m " +
+        "FROM Mission m " +
+        "LEFT JOIN Store s ON m.store.id = s.id " +
+        "LEFT JOIN Region r ON s.region.id = r.id " +
+        "WHERE r.name = :name")
+    List<Mission> findMissionsByRegionName(@Param("name") String name);
 
 }
