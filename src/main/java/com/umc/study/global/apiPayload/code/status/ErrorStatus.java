ad95139203a335aace5222262b20696f207db7f2
shin0112 <wnslcosltimo12@gmail.com>
2024-11-27 23:59:21 +0900

    feat: error handler 사용해보기

--- src/main/java/com/umc/study/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/study/global/apiPayload/code/status/ErrorStatus.java
@@ -18,6 +18,9 @@ public enum ErrorStatus implements BaseCode {
     // 회원
     _NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "MEMBER4001", "회원 정보 없음"),
     _ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4002", "이메일 중복"),
+
+    // 미션
+    _NOT_FOUND_MISSION_STATUS(HttpStatus.NOT_FOUND, "MISSION4001", "미션 상태 정보 없음"),
     ;
 
     private final HttpStatus httpStatus;
